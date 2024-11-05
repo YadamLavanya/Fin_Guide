beed672aa867ffa1160d663780868016425b6437
Adham Said <adham.afis@gmail.com>
2024-11-05 13:11:09 +0200

    git commit -m "feat: add login and register pages"

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import SignInFormDemo from '@/components/signin/signin-form'
+import { Sign } from 'crypto'
+import React from 'react'
+
+export default function Login() {
+  return (
+    <SignInFormDemo />
+  )
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+import SignupFormDemo from "@/components/signup/signup-form";
+
+export default function signup() {
+  return (
+    <div>
+      <SignupFormDemo />
+    </div>
+  );
+}
